e4a87ad413dcfca535eceebf4e7aedbbbc2dc17e
William Almeida <william.almeida@infracommerce.com.br>
2018-09-21 15:59:12 -0300

    [Backend Finished]

--- backend/src/api/todo/todo.js
+++ backend/src/api/todo/todo.js
@@ -1,7 +1,8 @@
 const restful = require('node-restful')
 const mongoose = restful.mongoose
 
-const todoSchema = new mongoose.todoSchema({
+
+const todoSchema = new mongoose.Schema({
     description: {
         type:String, required:true
     },
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,11 +1,15 @@
 const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
-const server = express();
+const server = express()
+const allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function(){
     console.log(`Backend is running in port ${port}.`);
 })
+
+module.exports = server
