7d9a54cf2c941e737b419594fc02cbd86b813cb7
William de Araujo Almeida <william.almeida9@fatec.sp.gov.br>
2018-12-12 14:12:17 -0200

    add configs

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
+require('dotenv').config()
 connectData = process.env.db || 'mongodb://localhost:todo'
 console.log(connectData)
+
 module.exports = mongoose.connect(connectData)
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,12 +1,19 @@
-const port = 8081
+const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
+const path = require('path');
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
 server.use(allowCors)
+
+const localPublic = 'frontend/public/';
+
+server.use(express.static(localPublic))
+
+server.use(express.static(path.join(__dirname, 'frontend/public')));
 
 server.listen(process.env.PORT || port, function(){
     console.log(`Backend is running in port ${port}.`);
