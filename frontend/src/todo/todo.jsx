18f58af5de4e5f99a679934b7972cf176c354c06
William de Araujo Almeida <william.almeida9@fatec.sp.gov.br>
2018-12-12 16:32:59 -0200

    change api

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -5,7 +5,7 @@ import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm';
 import TodoList from './todoList';
 
-const URL = 'http://localhost:3003/api/todos'
+const URL = process.env.api || 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component{
     constructor(props){        
